c7871be97d4064ebdd120d4058be29d051ab03e5
‎ ‎JIN <quocthao23061998.tg@icloud.com>
2026-08-27 16:00:47 +0700

    Enable searchable clipboard input

--- anotherNotch/components/Notch/AnotherNotchSkyLightWindow.swift
+++ anotherNotch/components/Notch/AnotherNotchSkyLightWindow.swift
@@ -109,6 +109,6 @@ class AnotherNotchSkyLightWindow: NSPanel {
     
     private var observers: Set<AnyCancellable> = []
     
-    override var canBecomeKey: Bool { false }
+    override var canBecomeKey: Bool { true }
     override var canBecomeMain: Bool { false }
 }
