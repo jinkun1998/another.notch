44de8ed6115216c211c5ef9b97f6d918896c9174
jinkun1998 <39690956+jinkun1998@users.noreply.github.com>
2026-09-09 08:32:48 +0700

    feat: add Homebrew cask

--- Casks/another-notch.rb
+++ Casks/another-notch.rb
@@ -1,15 +1,14 @@
-  cask "another-notch" do
-    version "1.1.0"
-    sha256 "deb90cea4a37e970da0f90af3d5cae63581114475625d87244c65c507e66a67c"
+cask "another-notch" do
+  version "1.1.0"
+  sha256 "deb90cea4a37e970da0f90af3d5cae63581114475625d87244c65c507e66a67c"
 
-    url
-    "https://github.com/jinkun1998/another.notch/releases/download/v#{version}/anotherNotch.dmg"
-    name "anotherNotch"
-    desc "Another Notch app for macOS"
-    homepage "https://github.com/jinkun1998/another.notch"
+  url "https://github.com/jinkun1998/another.notch/releases/download/v#{version}/anotherNotch.dmg"
+  name "anotherNotch"
+  desc "Another Notch app for macOS"
+  homepage "https://github.com/jinkun1998/another.notch"
 
-    auto_updates true
-    depends_on macos: ">= :sonoma"
+  auto_updates true
+  depends_on macos: ">= :sonoma"
 
-    app "anotherNotch.app"
-  end
+  app "anotherNotch.app"
+end
